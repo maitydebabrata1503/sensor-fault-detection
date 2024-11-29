800f24e7995b5e445e9768171f3c6a31a5aee839
maitydebabrata1503 <maitydebabrata1503@gmail.com>
2024-11-29 13:45:31 +0530

    executed

--- src/logger.py
+++ src/logger.py
@@ -2,12 +2,14 @@ import logging
 import os
 from datetime import datetime as dt 
 
-log_file_name = f"{dt.now().strftime("%d_%m_%Y_%H_%M_%s")}.log"
-log_file_path = os.path.join(os.getcwd(),"logs",log_file_name)
-os.makedirs(log_file_path, exist_ok = True)
+LOG_FILE = f" {dt.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path = os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_path, exist_ok = True)
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+
 
 logging.basicConfig(
-    filename=log_file_path,
-    format = "[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",                
-    level=logging.INFO
+    filename=LOG_FILE_PATH,
+    format = "[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",                
+    level=logging.INFO,
                     )
--- src/pipeline/training_pipeline.py
+++ src/pipeline/training_pipeline.py
@@ -2,12 +2,13 @@ import sys
 import os
 
 from src.components.Data_Ingestion import DataIngestion
-from src.components.Data_Transformation import DataTrasformation
+from src.components.Data_Transformation import DataTransformation
 from src.components.Model_trainer import ModelTrainer
 from src.exception import CustomException
 
 
 class TrainingPipeline:
+
     def start_data_ingestion(self):
         try:
             data_ingestion = DataIngestion()
@@ -16,9 +17,10 @@ class TrainingPipeline:
         except Exception as e:
             raise CustomException(e,sys)
         
+    
     def start_data_transformation(self,feature_store_file_path):
         try:
-            data_transformation = DataTrasformation(feature_store_file_path=feature_store_file_path)
+            data_transformation = DataTransformation(feature_store_file_path=feature_store_file_path)
             train_arr, test_arr, preprocessor_path = data_transformation.initiate_data_transformation()
             return train_arr, test_arr, preprocessor_path
         except Exception as e:
@@ -27,18 +29,22 @@ class TrainingPipeline:
     def start_model_training(self, train_arr,test_arr):
         try:
             model_trainer = ModelTrainer()
-            model_score = model_trainer.initiate_model_trainer(train_arr, test_arr)
+            model_score = model_trainer.initiate_model_trainer(
+                train_arr, test_arr
+            )
             return model_score
         except Exception as e:
             raise CustomException(e,sys)
         
-
     def run_pipeline(self):
         try:
             feature_store_file_path = self.start_data_ingestion()
-            train_arr, test_arr, preprocessor_path = self.start_data_transformation(feature_store_file_path)
-            accu_score = self.start_model_training(train_arr, test_arr)
-            print("training completed. Trained model score:", accu_score)
+            train_arr,test_arr,preprocessor_path = self.start_data_transformation(feature_store_file_path)
+            r2_square = self.start_model_training(train_arr,test_arr)
+
+            print("training completed. Trained model score:", r2_square)
+
         except Exception as e:
             raise CustomException(e,sys)
+
         
